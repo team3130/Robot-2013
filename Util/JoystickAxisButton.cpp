4a6fd35edd3e0fa6c4a2f54cbbc617069a76f92e
Matthew Ickstadt <Matthew.ickstadt@gmail.com>
2013-10-21 19:16:55 -0500

    Delete JoystickAxisButton.cpp

--- Util/JoystickAxisButton.cpp
+++
@@ -1,8 +0,0 @@
-#include "JoystickAxisButton.h"
-
-JoystickAxisButton::JoystickAxisButton(Joystick* joystick, int axis, float threshold) : m_joystick(joystick),
-	m_axis(axis), m_threshold(threshold) {}
-
-bool JoystickAxisButton::Get() {
-	return m_joystick->GetRawAxis(m_axis) > m_threshold;
-}
